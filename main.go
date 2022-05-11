dc25766540816b709de6bb6e1a0860a7aefa1d83
Nesquiko <lukascastven@gmail.com>
2022-05-11 21:19:43 +0200

    Remove test comment block.

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
-import "github.com/Nesquiko/go-auth/pkg/app"
+import (
+	"github.com/Nesquiko/go-auth/pkg/app"
+)
 
 func main() {
-	// password := []byte("123")
-
-	// // Hashing the password with the default cost of 10
-	// hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(string(hashedPassword))
-	// fmt.Printf("len is %d\n", len(hashedPassword))
-
-	// // Comparing the password with the hash
-	// err = bcrypt.CompareHashAndPassword(hashedPassword, password)
-	// fmt.Println(err) // nil means it is a match
 	app.StartServer()
 }
