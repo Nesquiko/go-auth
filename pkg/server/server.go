49581f03be1adf8591c369d3a1cea653313192f6
Nesquiko <lukascastven@gmail.com>
2022-05-11 21:21:25 +0200

    Connect to DB and fetch users.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,10 +1,28 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Nesquiko/go-auth/pkg/db"
+)
 
 type GoAuthServer struct{}
 
 func (s GoAuthServer) Signup(w http.ResponseWriter, r *http.Request) {
+	db, err := db.Connect("root", "goAuthDB", "users", "127.0.0.1:3306")
+	if err != nil {
+		panic(err)
+	}
+
+	users, err := db.FetchUsers()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, u := range users {
+		fmt.Println(u)
+	}
 
 }
 
