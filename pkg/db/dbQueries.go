64b3f054b1e931310b1de62fa9d68d454328170d
Nesquiko <lukascastven@gmail.com>
2022-05-12 14:15:09 +0200

    Add user by username and save user queries.

--- pkg/db/dbQueries.go
+++ pkg/db/dbQueries.go
@@ -1,6 +1,10 @@
 package db
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
 
 func (db Connection) FetchUsers() ([]User, error) {
 	var users []User
@@ -22,4 +26,39 @@ func (db Connection) FetchUsers() ([]User, error) {
 		return nil, fmt.Errorf("error: %v", err)
 	}
 	return users, nil
+}
+
+func (db Connection) UserByUsername(username string) (*User, error) {
+
+	var user User
+
+	row := db.QueryRow("SELECT * FROM users WHERE username = ?", username)
+
+	if err := row.Scan(&user.Uuid, &user.Username, &user.Email, &user.PasswordHash); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("user by %s: %v", username, err)
+		}
+		return nil, fmt.Errorf("user by %s: %v", username, err)
+	}
+	return &user, nil
+}
+
+func (db Connection) SaveUser(user *User) error {
+	result, err := db.Exec(
+		"INSERT INTO users (username, email, passwordHash) VALUES (?, ?, ?)",
+		user.Username,
+		user.Email,
+		user.PasswordHash,
+	)
+	if err != nil {
+		return fmt.Errorf("saveUser %s: %v", user.Username, err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return fmt.Errorf("saveUser %s: %v", user.Username, err)
+	}
+	log.Println(id)
+
+	return nil
 }
