af28ae17cbd6ea5899c30a6b363c21196fb48443
Nesquiko <lukascastven@gmail.com>
2022-05-19 13:09:21 +0200

    Respond with problem details.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,33 +1,50 @@
 package server
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
+	"github.com/Nesquiko/go-auth/pkg/api"
 	"github.com/Nesquiko/go-auth/pkg/db"
 )
 
 type GoAuthServer struct{}
 
 func (s GoAuthServer) Signup(w http.ResponseWriter, r *http.Request) {
-	db, closer, err := db.Connect("root", "goAuthDB", "users", "127.0.0.1:3306")
-	defer closer()
+	connection, err := db.Connect("root", "goAuthDB", "users", "127.0.0.1:3306")
+	if err != nil {
+		respondWithError(w, UNEXPECTED_ERROR)
+		return
+	}
 
+	var req api.SignupRequest
+	err = decodeJSONBody(w, r, &req)
 	if err != nil {
 		panic(err)
 	}
 
-	users, err := db.FetchUsers()
+	hashedPassword, err := encryptPassword(req.Password)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, u := range users {
-		fmt.Println(u)
+	newUser := &db.UserModel{
+		Username:     req.Username,
+		Email:        req.Email,
+		PasswordHash: hashedPassword,
 	}
 
+	connection.SaveUser(newUser)
 }
 
 func (s GoAuthServer) Login(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func respondWithError(w http.ResponseWriter, problem api.ProblemDetails) {
+
+	w.Header().Set(CONTENT_TYPE, APPLICATION_JSON)
+	w.WriteHeader(problem.StatusCode)
+
+	json.NewEncoder(w).Encode(problem)
 }
