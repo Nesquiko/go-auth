3424c8d103550d4abb4dbec16655a7ca946523b9
Nesquiko <lukascastven@gmail.com>
2022-05-20 22:45:41 +0200

    Add problem detail for bad json request.

--- pkg/server/problemDetails.go
+++ pkg/server/problemDetails.go
@@ -6,9 +6,18 @@ import (
 	"github.com/Nesquiko/go-auth/pkg/api"
 )
 
-var UNEXPECTED_ERROR = api.ProblemDetails{
+var UnexpectedError = api.ProblemDetails{
 	StatusCode: http.StatusInternalServerError,
 	Type:       "unexpected.error",
 	Title:      "Unexpected error occured",
 	Detail:     "An unexpected error occured during processing your request",
+}
+
+func BadRequest(err malformedRequest) api.ProblemDetails {
+	return api.ProblemDetails{
+		StatusCode: err.status,
+		Type:       "bad.request",
+		Title:      "Bad request",
+		Detail:     err.msg,
+	}
 }
