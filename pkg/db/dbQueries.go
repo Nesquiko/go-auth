1e1265b9ac38d8186ba4fa0d3015bc4ec5488214
Nesquiko <lukascastven@gmail.com>
2022-05-21 12:53:52 +0200

    Cleanup queries.

--- pkg/db/dbQueries.go
+++ pkg/db/dbQueries.go
@@ -3,33 +3,9 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"log"
 )
 
-func (db connection) FetchUsers() ([]UserDBEntity, error) {
-	var users []UserDBEntity
-
-	rows, err := db.Query("SELECT * FROM users")
-	if err != nil {
-		return nil, fmt.Errorf("error from query: %v", err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var user UserDBEntity
-		if err := rows.Scan(&user.Uuid, &user.Username, &user.Email, &user.PasswordHash); err != nil {
-			return nil, fmt.Errorf("error in scanning: %v", err)
-		}
-		users = append(users, user)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("error: %v", err)
-	}
-	return users, nil
-}
-
 func (db connection) UserByUsername(username string) (*UserDBEntity, error) {
-
 	var user UserDBEntity
 
 	row := db.QueryRow("SELECT * FROM users WHERE username = ?", username)
@@ -44,7 +20,7 @@ func (db connection) UserByUsername(username string) (*UserDBEntity, error) {
 }
 
 func (db connection) SaveUser(user *UserModel) error {
-	result, err := db.Exec(
+	_, err := db.Exec(
 		"INSERT INTO users (username, email, passwordHash) VALUES (?, ?, ?)",
 		user.Username,
 		user.Email,
@@ -53,12 +29,6 @@ func (db connection) SaveUser(user *UserModel) error {
 	if err != nil {
 		return fmt.Errorf("saveUser %s: %v", user.Username, err)
 	}
-
-	id, err := result.LastInsertId()
-	if err != nil {
-		return fmt.Errorf("saveUser %s: %v", user.Username, err)
-	}
-	log.Println(id)
 
 	return nil
 }
