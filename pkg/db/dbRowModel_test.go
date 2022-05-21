7a93e21c101fb102fe60a5b104db811d098dbd74
Nesquiko <lukascastven@gmail.com>
2022-05-21 12:47:02 +0200

    Test db row model.

---
+++ pkg/db/dbRowModel_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestUserDBEntity_String(t *testing.T) {
+	id, _ := uuid.NewUUID()
+	username, email, passwdHash := "Johny", "john@foo.bar", "$asdfioj5641684"
+	want := fmt.Sprintf("username: %s | email: %s | uuid: %s", username, email, id.String())
+
+	u := UserDBEntity{Uuid: id, Username: username, Email: email, PasswordHash: passwdHash}
+
+	if got := u.String(); got != want {
+		t.Errorf("UserDBEntity.String() = %v, want %v", got, want)
+	}
+}
+
+func TestUserModel_String(t *testing.T) {
+	username, email, passwdHash := "Johny", "john@foo.bar", "$asdfioj5641684"
+	want := fmt.Sprintf("username: %s | email: %s", username, email)
+
+	u := UserModel{Username: username, Email: email, PasswordHash: passwdHash}
+	if got := u.String(); got != want {
+		t.Errorf("UserModel.String() = %v, want %v", got, want)
+	}
+}
