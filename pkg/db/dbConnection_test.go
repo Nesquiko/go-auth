6d94bcf34b411f44e8da764a0d02611ec8996ddf
Nesquiko <lukascastven@gmail.com>
2022-05-21 13:19:23 +0200

    Restructure tests on connection to db and add one.

--- pkg/db/dbConnection_test.go
+++ pkg/db/dbConnection_test.go
@@ -4,6 +4,92 @@ import (
 	"testing"
 )
 
+func TestConnectDBIsNil(t *testing.T) {
+	if DBConnection != nil {
+		t.Error("Uninitialized connection to db is not nil")
+	}
+}
+
+func TestConnectDBErrors(t *testing.T) {
+	type args struct {
+		driver string
+		dsn    string
+	}
+
+	conFailMsg := "Connection should be nil to indicate error"
+	errFailMsg := "Err cannot be nil"
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "InvalidDSN",
+			args: args{driver: "mysql", dsn: "invalidDSN"},
+		},
+		{
+			name: "InvalidDriver",
+			args: args{driver: "invalid", dsn: "root:passwd@tcp(127.0.0.1:3306)/users?"},
+		},
+		{
+			name: "ConnectionRefused",
+			args: args{driver: "mysql", dsn: "root:passwd@tcp(127.0.0.1:3306)/users?"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ConnectDB(tt.args.driver, tt.args.dsn)
+
+			if DBConnection != nil {
+				t.Error(conFailMsg)
+			}
+			if err == nil {
+				t.Error(errFailMsg)
+			}
+		})
+	}
+}
+
+func Test_connectErrors(t *testing.T) {
+	type args struct {
+		driver string
+		dsn    string
+	}
+
+	conFailMsg := "Connection should be nil to indicate error"
+	errFailMsg := "Err cannot be nil"
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "InvalidDSN",
+			args: args{driver: "mysql", dsn: "invalidDSN"},
+		},
+		{
+			name: "InvalidDriver",
+			args: args{driver: "invalid", dsn: "root:passwd@tcp(127.0.0.1:3306)/users?"},
+		},
+		{
+			name: "ConnectionRefused",
+			args: args{driver: "mysql", dsn: "root:passwd@tcp(127.0.0.1:3306)/users?"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, err := connect(tt.args.driver, tt.args.dsn)
+
+			if c != nil {
+				t.Error(conFailMsg)
+			}
+			if err == nil {
+				t.Error(errFailMsg)
+			}
+		})
+	}
+}
+
 func TestMySQLDSNConfig(t *testing.T) {
 	user := "testUser"
 	passwd := "testPasswd"
@@ -23,38 +109,5 @@ func TestMySQLDSNConfig(t *testing.T) {
 	}
 	if cfg.DBName != dbname {
 		t.Errorf("Expected dbname to be %s, but was %s", dbname, cfg.DBName)
-	}
-}
-
-func Test_connectInvalidDSN(t *testing.T) {
-	c, err := connect("mysql", "invalidDSN")
-
-	if c != nil {
-		t.Error("Connection should be nil to indeicate error")
-	}
-	if err == nil {
-		t.Error("Err cannot be nil")
-	}
-}
-
-func Test_connectInvalidDriver(t *testing.T) {
-	c, err := connect("invalid", "root:passwd@tcp(127.0.0.1:3306)/users?")
-
-	if c != nil {
-		t.Error("Connection should be nil to indeicate error")
-	}
-	if err == nil {
-		t.Error("Err cannot be nil")
-	}
-}
-
-func Test_connectConnectionRefused(t *testing.T) {
-	c, err := connect("mysql", "root:passwd@tcp(127.0.0.1:3306)/users?")
-
-	if c != nil {
-		t.Error("Connection should be nil to indeicate error")
-	}
-	if err == nil {
-		t.Error("Err cannot be nil")
 	}
 }
