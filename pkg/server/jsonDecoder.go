9cefc6a7b080a575cd56c12bb0548550664a37e6
Nesquiko <lukascastven@gmail.com>
2022-05-23 16:36:09 +0200

    Test friendly max json body size.

--- pkg/server/jsonDecoder.go
+++ pkg/server/jsonDecoder.go
@@ -14,9 +14,9 @@ import (
 const (
 	contentType     = "Content-Type"
 	applicationJSON = "application/json"
-
-	maxSize = 1024
 )
+
+var maxSize = 1024
 
 type malformedRequest struct {
 	status int
@@ -34,7 +34,7 @@ func decodeJSONBody[T any](w http.ResponseWriter, r *http.Request, dest T) error
 		return malformedRequest{status: http.StatusUnsupportedMediaType, msg: responseMsg}
 	}
 
-	r.Body = http.MaxBytesReader(w, r.Body, maxSize)
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxSize))
 
 	dec := json.NewDecoder(r.Body)
 	dec.DisallowUnknownFields()
