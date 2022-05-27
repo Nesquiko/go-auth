3e5276e27cd2bbdd598bc862816f2b5afd99835d
Nesquiko <lukascastven@gmail.com>
2022-05-27 16:49:03 +0200

    Map as db in db mock.

--- pkg/db/mocks/dbConnMock.go
+++ pkg/db/mocks/dbConnMock.go
@@ -1,15 +1,43 @@
 package mocks
 
-import "github.com/Nesquiko/go-auth/pkg/db"
+import (
+	"database/sql"
+
+	"github.com/Nesquiko/go-auth/pkg/db"
+	"github.com/go-sql-driver/mysql"
+)
 
 type DBConnectionMock struct {
 }
 
-func (db DBConnectionMock) UserByUsername(username string) (*db.UserDBEntity, error) {
+var fakeDB = make(map[string]*db.UserDBEntity, 0)
 
-	return nil, nil
+func (db DBConnectionMock) UserByUsername(username string) (*db.UserDBEntity, error) {
+	user, ok := fakeDB[username]
+	if !ok {
+		return nil, sql.ErrNoRows
+	}
+
+	return user, nil
 }
 
 func (db DBConnectionMock) SaveUser(user *db.UserModel) error {
+
+	if _, ok := fakeDB[user.Username]; ok {
+		return &mysql.MySQLError{
+			Number:  1062,
+			Message: "duplicate entries for users.username",
+		}
+	}
+
+	for _, v := range fakeDB {
+		if v.Email == user.Email {
+			return &mysql.MySQLError{
+				Number:  1062,
+				Message: "duplicate entries for users.email",
+			}
+		}
+	}
+
 	return nil
 }
