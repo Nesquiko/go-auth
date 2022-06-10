1ac36e97bb68531e9cfa057a3aa69ab9ff9a67f1
Nesquiko <lukascastven@gmail.com>
2022-06-10 19:36:28 +0200

    If bearer is present, dont check content type.
    Signed-off-by: Nesquiko <lukascastven@gmail.com>

--- pkg/middleware/contentType.go
+++ pkg/middleware/contentType.go
@@ -8,11 +8,18 @@ import (
 )
 
 // ContentTypeFilter is a middleware for filtering requests which do not have
-// Content-Type header set to application/json.
+// Content-Type header set to application/json. Firstly checks if any bearer
+// token is in Headers, if yes, then proceed, if no check for correct Content-Type
+// header.
 func ContentTypeFilter(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if ct := r.Header.Get(consts.ContentType); ct != consts.ApplicationJSON {
+
+		if bearer := r.Header.Get(consts.Authorization); bearer != "" {
+			next.ServeHTTP(w, r)
+			return
+
+		} else if ct := r.Header.Get(consts.ContentType); ct != consts.ApplicationJSON {
 			responseMsg := "Content-Type header is not application/json"
 
 			pd := api.ProblemDetails{
