2f6b8b1dafffe82aa9985b1ea86508673c52aeed
Nesquiko <lukascastven@gmail.com>
2022-06-11 15:28:31 +0200

    Update sql databse.
    Signed-off-by: Nesquiko <lukascastven@gmail.com>

--- SQL/CreateTable.sql
+++ SQL/CreateTable.sql
@@ -1,7 +1,11 @@
+CREATE DATABASE users;
+USE users;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     uuid VARCHAR(36) DEFAULT (uuid()) NOT NULL PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
     email VARCHAR(320) NOT NULL UNIQUE,
-    passwordHash CHAR(60) BINARY NOT NULL
+    passwordHash CHAR(60) BINARY NOT NULL,
+	secret2FA CHAR(16),
+	enabled2FA BIT DEFAULT 0
 );
